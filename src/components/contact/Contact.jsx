c9aeae4e0a3c4a6499f1ac2843af089ee205eb32
donnishb <donnishmac@gmail.com>
2022-02-21 10:36:15 +0000

    1stCommit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,45 @@
+import { useState,useRef } from "react";
+import emailjs from '@emailjs/browser';
+import "./contact.scss";
+
+
+export default function Contact() {
+  const form = useRef();
+  const [message, setMessage] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+        emailjs.sendForm('service_uqz7hx6', 'template_fwfeqfy', form.current, 'user_kGfkxhe3D99EIDvRjHelX')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          setMessage(true);
+          form.current.reset();
+      }
+      
+    
+ 
+
+  /*const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };*/
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="Name" />
+          <input type="text" placeholder="Email" name="Email"/>
+          <textarea placeholder="Message" name="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span><h1>Thanks, I'll reply ASAP :)</h1></span>}
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,38 @@
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Web Developer", "Mobile Developer", "Database Designer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.jpg" alt="Godfred A. Mintah" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Godfred A. Mintah</h1>
+          <h3>
+            A Professional <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
